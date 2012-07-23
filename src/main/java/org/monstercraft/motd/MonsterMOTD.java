295fa4571775e895c3421c868ecd1cc761c57bbe
Matt Baxter <matt@phozop.net>
2012-07-23 16:39:51 -0400

    Fixed, minus formatting

--- src/main/java/org/monstercraft/motd/MonsterMOTD.java
+++ src/main/java/org/monstercraft/motd/MonsterMOTD.java
@@ -1,19 +1,25 @@
 package org.monstercraft.motd;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.monstercraft.motd.plugin.Configuration;
-import org.monstercraft.motd.plugin.Configuration.Variables;
-import org.monstercraft.motd.plugin.managers.CommandManager;
-import org.monstercraft.motd.plugin.managers.HandleManager;
-import org.monstercraft.motd.plugin.managers.HookManager;
-import org.monstercraft.motd.plugin.managers.SettingsManager;
-import org.monstercraft.motd.plugin.reflection.Reflect;
 import org.monstercraft.motd.plugin.util.Metrics;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 /**
  * This class represents the main plugin. All actions related to the plugin are
  * forwarded by this class
@@ -21,53 +27,33 @@ import org.monstercraft.motd.plugin.util.Metrics;
  * @author Fletch_to_99 <fletchto99@hotmail.com>
  * 
  */
-public class MonsterMOTD extends JavaPlugin implements Runnable {
+public class MonsterMOTD extends JavaPlugin implements Listener {
 
-	/**
-	 * Enables the plugin.
-	 */
-
-	private static SettingsManager settings = null;
-
-	private static CommandManager command = null;
-
-	private static HookManager hooks = null;
-
-	private static HandleManager handles = null;
+    private final String DENIED = "You do not have permission for this command";
+    
+    private String currentMOTD;
+    private String defaultMOTD;
 
 	public void onEnable() {
-		try {
-			Variables.defaultMOTD = Reflect.getMotd();
-		} catch (Exception e) {
-			log("Error setting up!");
-			debug(e);
+	    if(!new File(this.getDataFolder(),"config.yml").exists()){
+	        this.saveDefaultConfig();
+	    }
+		this.currentMOTD = this.getConfig().getString("CURRENT-MOTD");
+		this.defaultMOTD = this.getConfig().getString("DEFAULT-MOTD");
+		if(this.defaultMOTD == null) {
+		    this.defaultMOTD = "A Minecraft server";
 		}
-		hooks = new HookManager(this);
-		handles = new HandleManager();
-		command = new CommandManager();
-		settings = new SettingsManager(this);
-		Thread t = new Thread(this);
-		t.setDaemon(true);
-		t.setPriority(Thread.MAX_PRIORITY);
-		t.start();
-	}
-
-	/**
-	 * Disables the plugin.
-	 */
-
-	public void onDisable() {
-		log("Disabling MonsterMOTD....");
-		log("Setting back the default MOTD....");
-		try {
-			Reflect.setMotd(Variables.defaultMOTD);
-		} catch (Exception e) {
+		if(this.currentMOTD == null) {
+		    this.currentMOTD = "A Minecraft server";
 		}
-		log("Successfully disabled MonsterMOTD");
+		try {
+            new Metrics(this).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.getServer().getScheduler().scheduleAsyncDelayedTask(this, new Update(this.getDescription().getVersion()));
 	}
-
-	private final static Logger logger = Logger.getLogger(MonsterMOTD.class
-			.getSimpleName());
 
 	/**
 	 * Logs a message to the console.
@@ -75,8 +61,8 @@ public class MonsterMOTD extends JavaPlugin implements Runnable {
 	 * @param msg
 	 *            The message to print.
 	 */
-	public static void log(final String msg) {
-		logger.log(Level.INFO, "[MonsterMOTD] " + msg);
+	public void log(String msg) {
+		this.getLogger().info(msg);
 	}
 
 	/**
@@ -85,84 +71,102 @@ public class MonsterMOTD extends JavaPlugin implements Runnable {
 	 * @param error
 	 *            The message to print.
 	 */
-	public static void debug(final String error, final boolean console) {
-		if (console) {
-			logger.log(Level.WARNING, "[MonsterMOTD - Debug] " + error);
-		}
+	public void debug(final Exception error) {
+	    this.getLogger().log(Level.SEVERE, "Critical error detected!", error);
 	}
-
-	/**
-	 * Logs debugging messages to the console.
-	 * 
-	 * @param error
-	 *            The message to print.
-	 */
-	public static void debug(final Exception error) {
-		logger.log(Level.SEVERE, "[MonsterIRC - Critical error detected!]");
-		error.printStackTrace();
+	
+	private void setMOTD(String motd) {
+	    this.currentMOTD = motd == null ? this.defaultMOTD : motd;
+	    this.getConfig().set("CURRENT-MOTD", this.currentMOTD);
+	    this.saveConfig();
 	}
 
 	/**
 	 * Handles commands.
 	 */
-
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-		return getCommandManager().onGameCommand(sender, command, label, args);
+	    if(args.length > 1 && args[0].equalsIgnoreCase("clear")){
+	        if(!sender.hasPermission("monstermotd.view")){
+	            sender.sendMessage(DENIED);
+	            return true;
+	        }
+	        this.setMOTD(null);
+	        sender.sendMessage(ChatColor.GREEN
+	                + "MOTD successfully set back to the default: " + ChatColor.RESET + this.defaultMOTD);
+	        return true;
+	    }
+	    if(args.length>1 && args[0].equalsIgnoreCase("set")){
+	        if(!sender.hasPermission("monstermotd.set")){
+	            sender.sendMessage(DENIED);
+	            return true;
+	        }
+	        if (args.length < 2) {
+	            sender.sendMessage(ChatColor.RED + "You must put a MOTD to set!");
+	            return true;
+	        }
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 1; i < args.length; i++) {
+	            sb.append(args[i]);
+	            sb.append(" ");
+	        }
+	        sb.setLength(sb.length()-1);
+	        String motd = sb.toString();
+	        this.setMOTD(motd);
+	        sender.sendMessage(ChatColor.GREEN + "MOTD successfully set to: "
+	                + ChatColor.RESET + motd);
+	        return true;
+	    }
+	    // Else, display motd.
+	    if(sender.hasPermission("monstermotd.view")){
+	            sender.sendMessage(ChatColor.GREEN + "Todays MOTD: " + ChatColor.RESET
+                    + this.currentMOTD);
+	    }
+	    sender.sendMessage(DENIED);
+        return true;
+	}
+	
+	@EventHandler
+	public void onPing(ServerListPingEvent event) {
+	    event.setMotd(this.currentMOTD);
 	}
 
-	/**
-	 * The manager that creates the hooks with other plugins.
-	 * 
-	 * @return The hooks.
-	 */
-	public static HookManager getHookManager() {
-		return hooks;
-	}
+	private class Update implements Runnable {
+	    private final String version;
 
-	/**
-	 * The manager that holds the handlers.
-	 * 
-	 * @return The handlers.
-	 */
-	public static HandleManager getHandleManager() {
-		return handles;
+        public Update(String version) {
+	        this.version = version;
+	    }
+        
+        public void run() {
+                try {
+                    URL url = new URL("http://dev.bukkit.org/server-mods/monstermotd/files.rss");
+                    Document doc = DocumentBuilderFactory.newInstance()
+                            .newDocumentBuilder()
+                            .parse(url.openConnection().getInputStream());
+                    doc.getDocumentElement().normalize();
+                    NodeList nodes = doc.getElementsByTagName("item");
+                    Node firstNode = nodes.item(0);
+                    if (firstNode.getNodeType() == 1) {
+                        Element firstElement = (Element) firstNode;
+                        NodeList firstElementTagName = firstElement
+                                .getElementsByTagName("title");
+                        Element firstNameElement = (Element) firstElementTagName
+                                .item(0);
+                        NodeList firstNodes = firstNameElement.getChildNodes();
+                        final String cur = firstNodes.item(0).getNodeValue();
+                        if(cur.contains(version)){
+                            log("You are using the latest version of MonsterMOTD");
+                        } else {
+                            log(cur + " is out! You are running " +version);
+                            log("Update MonsterMOTD at: http://dev.bukkit.org/server-mods/monstermotd");
+                        }
+                    }
+                } catch (Exception e) {
+                    debug(e);
+                }
+                
+        }
 	}
-
-	/**
-	 * The plugins settings.
-	 * 
-	 * @return The settings.
-	 */
-	protected static SettingsManager getSettingsManager() {
-		return settings;
-	}
-
-	/**
-	 * The CommandManager that Assigns all the commands.
-	 * 
-	 * @return The plugins command manager.
-	 */
-	protected static CommandManager getCommandManager() {
-		return command;
-	}
-
-	public void run() {
-		try {
-			log("Setting up metrics!");
-			Metrics metrics = new Metrics(this);
-			metrics.start();
-			String newVersion = Configuration.checkForUpdates(this,
-					Configuration.URLS.UPDATE_URL);
-			if (!newVersion.contains(Configuration.getCurrentVerison(this))) {
-				log(newVersion + " is out! You are running "
-						+ Configuration.getCurrentVerison(this));
-				log("Update MonsterMOTD at: http://dev.bukkit.org/server-mods/monstermotd");
-			} else {
-				log("You are using the latest version of MonsterMOTD");
-			}
-		} catch (Exception e) {
-			debug(e);
-		}
-	}
+	
 }
